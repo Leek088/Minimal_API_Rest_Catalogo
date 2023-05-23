0bd3a541309d2d2e18c6b4c1068359270ad83c85
Leeko88 <Leeko88@POSITIVO-PC>
2023-05-23 13:29:26 -0300

    Adicionar arquivos de projeto.

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalApiRest.Models;
+
+namespace MinimalApiRest.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
+
+    //Usando a fluente api para gerenciar as propriedades das entidades
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //Entidade Categoria
+        modelBuilder.Entity<Categoria>().HasKey(c => c.CategoriaId);
+        modelBuilder.Entity<Categoria>().Property(c => c.Nome).HasMaxLength(100).IsRequired();
+        modelBuilder.Entity<Categoria>().Property(c => c.Descricao).HasMaxLength(150).IsRequired();
+
+        //Entidade Produto
+        modelBuilder.Entity<Produto>().HasKey(p => p.ProdutoId);
+        modelBuilder.Entity<Produto>().Property(p => p.Nome).HasMaxLength(100).IsRequired();
+        modelBuilder.Entity<Produto>().Property(p => p.Descricao).HasMaxLength(150);
+        modelBuilder.Entity<Produto>().Property(p => p.Imagem).HasMaxLength(100);
+        modelBuilder.Entity<Produto>().Property(p => p.Preco).HasPrecision(14, 2);
+
+        //Relacionamento
+        modelBuilder.Entity<Produto>()
+            .HasOne<Categoria>(c => c.Categoria)
+            .WithMany(p => p.Produtos)
+            .HasForeignKey(c => c.CategoriaId);
+
+    }
+
+}
---
+++ Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿namespace MinimalApiRest.Models;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
+
+    //Relação de navegação
+    public ICollection<Produto>? Produtos { get; set; }
+}
