f03ae8cc43995f418b533dbb17a84570778e7259
Leek088 <leonardoleeko88@gmail.com>
2023-05-24 10:58:10 -0300

    Ver 1.1 Criação da migração Configuração da conexão Mysql Inclusão da DI do serviço do Contexto.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApiRest.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Recuperar a string de conexão
+var connectionStringMyql = builder.Configuration.GetConnectionString("ConnectionMysql");
+
+// Incluir o serviço de contexto.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionStringMyql, ServerVersion.AutoDetect(connectionStringMyql)));
 
 var app = builder.Build();
 
